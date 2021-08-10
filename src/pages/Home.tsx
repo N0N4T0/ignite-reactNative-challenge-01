c4573330f2cbbcae8d03a1a1e38e18be3d7b8a66
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-08-09 23:19:35 -0300

    removendo task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,6 +25,9 @@ export function Home() {
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(updatedTasks)
   }
 
   return (
